0aa4c1323901beaa55b17203152d059943ab0181
pranshulpradhan <139995434+pranshulpradhan@users.noreply.github.com>
2023-11-02 00:31:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,100 +1,74 @@
-const searchInput = document.getElementById('searchInput');
-const moviesContainer = document.getElementById('movies');
-const paginationContainer = document.getElementById('pagination');
+const searchInput = document.getElementById("searchInput");
+const moviesCards =document.getElementById("movies");
+const paginationPart = document.getElementById("pagination");
+
+const apiUrl = "https://www.omdbapi.com/";
+const API_KEY = "4dbe716f";
 
 let currentPage = 1;
-let totalResults = 0;
+let totalPage = 0;
+let debounceTimer;
 
-const API_KEY = '4dbe716f'; 
-const BASE_URL = 'https://www.omdbapi.com/';
+searchInput.addEventListener("input",() => {
+    clearTimeout(debounceTimer);
+    debounceTimer = setTimeout(searchMovies, 500);
+})
 
-const debounce = (func, delay) => {
-  let timeoutId;
-  return (...args) => {
-    if (timeoutId) {
-      clearTimeout(timeoutId);
-    }
-    timeoutId = setTimeout(() => {
-      func(...args);
-    }, delay);
-  };
-};
+const getData = async(searchTerm,page) => {
+    console.log("searchTerm", searchTerm);
+    console.log("page", page);
+    const response = await fetch(`${apiUrl}?&apikey=${API_KEY}&s=${searchTerm}&page=${page}`);
+    console.log("response", response);
+    const data = await response.json();
+    console.log("getData",data);
+    return data;
+}
 
-const fetchMovies = async (searchTerm, page) => {
-  const response = await fetch(`${BASE_URL}?&apikey=${API_KEY}&s=${searchTerm}&page=${page}`);
-  console.log(response);
-  const data = await response.json();
-  console.log(data);
-  return data;
-};
-
-const displayMovies = (movies) => {
-  moviesContainer.innerHTML = '';
-  console.log(movies.Search);
-  movies.Search.forEach(movie => {
-    const movieCard = document.createElement('div');
-    movieCard.classList.add('movie-card');
-    movieCard.innerHTML = `
-      <img src="${movie.Poster?movie.Poster:"https://www.shutterstock.com/shutterstock/photos/586719869/display_1500/stock-vector-online-cinema-art-movie-watching-with-popcorn-and-film-strip-cinematograph-concept-vintage-retro-586719869.jpg"}" alt="${movie.Title}">
+const displayMovies = async movieDetails => {
+    moviesCards.innerHTML='';
+    console.log("display",movieDetails);
+    const movieContainer = document.createElement('div');
+    // movieContainer.classList.add(movie-container);
+    await movieDetails.Search.forEach(movie => {
+        console.log("movie",movie);
+        const movieCard = document.createElement('div');
+        // movieCard.classList.add("movie-details");
+        movieCard.innerHTML=`
+        <img src="${movie.Poster?movie.Poster:"https://www.shutterstock.com/shutterstock/photos/586719869/display_1500/stock-vector-online-cinema-art-movie-watching-with-popcorn-and-film-strip-cinematograph-concept-vintage-retro-586719869.jpg"}" alt="${movie.Title}">
       <h3>${movie.Title}</h3>
       <p>Year: ${movie.Year}</p>
-    `;
-    moviesContainer.appendChild(movieCard);
-  });
-};
+        `;
+        movieContainer.appendChild(movieCard);
+        moviesCards.appendChild(movieContainer);
+    });
+    return;
+}
 
 const displayPagination = () => {
-  const totalPages = Math.ceil(totalResults / 10);
-  paginationContainer.innerHTML = `
+    const totalPages = Math.ceil(totalPage / 10);
+    console.log("totalPages",totalPages);
+    paginationPart.innerHTML=`
     <button ${currentPage === 1 ? 'disabled' : ''} onclick="goToPage(${currentPage - 1})">Previous</button>
     <span>Page ${currentPage} of ${totalPages}</span>
     <button ${currentPage === totalPages ? 'disabled' : ''} onclick="goToPage(${currentPage + 1})">Next</button>
-  `;
-};
+    `;
+}
 
 const goToPage = (page) => {
-  if (page >= 1 && page <= Math.ceil(totalResults / 10)) {
-    currentPage = page;
-    searchMovies();
-  }
-};
+    console.log("goto",totalPage);
+    if (page >= 1 && page <= Math.ceil(totalPage / 10)) {
+      currentPage = page;
+      console.log("gotoPage",currentPage);
+      searchMovies();
+    }
+  };
 
 const searchMovies = async () => {
     const searchTerm = searchInput.value;
-    if (searchTerm.trim() === '') {
-      moviesContainer.innerHTML = '<p>Please enter a search term</p>';
-      paginationContainer.innerHTML = '';
-      return;
-    }
-  
-    const data = await fetchMovies(searchTerm, currentPage);
-    
-    if (data.Response === 'False') {
-      if (data.Error === 'Too many results.') {
-        moviesContainer.innerHTML = '<p>Too many results. Please provide a more specific search term.</p>';
-        paginationContainer.innerHTML = '';
-      } else {
-        moviesContainer.innerHTML = `<p>Error: ${data.Error}</p>`;
-        paginationContainer.innerHTML = '';
-      }
-      return;
-    }
-    
-    totalResults = parseInt(data.totalResults);
-    
-    if (totalResults === 0) {
-      moviesContainer.innerHTML = '<p>No results found</p>';
-      paginationContainer.innerHTML = '';
-      return;
-    }
-    
-    displayMovies(data);
+    const movieDetails = await getData(searchTerm,currentPage);
+    console.log("searchMovies data", movieDetails);
+    totalPage = parseInt(movieDetails.totalResults);
+    console.log("totalPages searchMovies", totalPage);
+    displayMovies(movieDetails);
     displayPagination();
-  };
-  
-
-const debouncedSearchMovies = debounce(searchMovies, 300);
-
-searchInput.addEventListener('input', debouncedSearchMovies);
-
-searchMovies();
+}
